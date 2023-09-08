abff915f696651a59d4bbbe260d2d8adf428daca
Jpleite15 <144491852+Jpleite15@users.noreply.github.com>
2023-09-08 17:55:01 -0300

    test

--- JAVA - APRENDIZADO/Aula1/exercicioAula1/Main.java
+++ JAVA - APRENDIZADO/Aula1/exercicioAula1/Main.java
@@ -3,5 +3,8 @@ package exercicioAula1;
 public class Main {
     public static void main(String[] args) {
         System.out.println("a");
+        System.out.println("b");
+        System.out.println("b");
+        
     }
 }
