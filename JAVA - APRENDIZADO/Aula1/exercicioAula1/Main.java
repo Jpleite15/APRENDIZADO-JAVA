7178ff6932b6e1ef0c3309a76aa0a9da22e8fb9e
Jpleite15 <144491852+Jpleite15@users.noreply.github.com>
2023-09-08 17:56:25 -0300

    Update Main.java

--- JAVA - APRENDIZADO/Aula1/exercicioAula1/Main.java
+++ JAVA - APRENDIZADO/Aula1/exercicioAula1/Main.java
@@ -4,7 +4,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("a");
         System.out.println("b");
-        System.out.println("b");
-        
     }
 }
