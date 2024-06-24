af34dadd5185e2f105ad3fd1b7b53343ced74b87
Jpleite15 <144491852+Jpleite15@users.noreply.github.com>
2024-06-24 16:36:24 -0300

    Update Main.java

--- Secao 04/Exercicio02/src/Main.java
+++ Secao 04/Exercicio02/src/Main.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc =  new Scanner(System.in);
-        System.out.println(" digite o valor  do raio ");
+        System.out.print(" digite o valor  do raio : ");
         double x;
         double y;
         x = sc.nextDouble();
